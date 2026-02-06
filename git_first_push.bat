8a3bc8f42c246074629891f8ff330b02fff5b9ab
Y Kim <ykim7389@naver.com>
2026-02-06 10:42:23 -0600

    Auto commit at 2026-02-06_10-42-15

--- git_first_push.bat
+++ git_first_push.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "first commit"
+git commit -m "First commit"
 git branch -M main
 git remote add origin https://github.com/ykim2718/CPP.git
 git push -u origin main
